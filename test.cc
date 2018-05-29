f5c8abb786f6ca8097ee69669d6f15850db380f2
XiaoTianCan <gn16@mails.tsinghua.edu.cn>
2018-05-29 16:26:38 +0800

    first commit

---
+++ test.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/ipv4-global-routing-helper.h"
+
+using namespace ns3;
+using namespace std;
+
+NS_LOG_COMPONENT_DEFINE ("TopoFour");
+
+int main (int argc, char *argv[])
+{
+  NS_LOG_UNCOND ("Hello!");
+  return 0;
+}
